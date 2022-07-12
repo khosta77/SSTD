498f512c21fb84db66d86ce1a49178b1c7876c1b
khosta77 <puwerfulpants@gmail.com>
2022-07-12 18:06:26 +0300

    Начал писать вектор с шаблонами

--- project/include/stepanvector.h
+++ project/include/stepanvector.h
@@ -3,35 +3,134 @@
 
 #include <iostream>
 
+template<typename T>
 struct component_vector {
-    int data;
-    component_vector *next;
-    component_vector *preveus;
+    T data;
+    component_vector<T> *next;
+    component_vector<T> *preveus;
 };
 
+template<typename T>
 class StepanVector {
 private:
-    component_vector *first;
-    component_vector *last;
-    int count;
-protected:
-    component_vector *previous(component_vector *node);
-    component_vector *next(component_vector *node);
-    component_vector *get_first();
-    component_vector *get_last();
+    component_vector<T> *first;
+    component_vector<T> *last;
+    size_t count{};
+
+//protected:
+//    component_vector<T> *previous(component_vector *node);
+//    component_vector<T> *next(component_vector *node);
+//    component_vector<T> *get_first();
+//    component_vector<T> *get_last();
+
 public:
-    StepanVector();
-    int get_value(component_vector *node);
-    int get_count();
-    bool empty();
-    component_vector *push(int data, component_vector *node);
-    component_vector *clear_node(component_vector *node);
-    component_vector *at(int i);
-    void print(bool tail = true);
-    void push_first(int data);
-    void push_last(int data);
-    void clear();
-    ~StepanVector();
+    // Constructor & destructor
+    StepanVector() {
+        first = last = NULL;
+        count = 0;
+    }
+
+//    StepanVector (const StepanVector<T> &SV);
+//    StepanVector<T> &operator= (const StepanVector<T> &SV);
+//    ~StepanVector();
+
+    // Iterators
+
+    // Capacity
+
+    // Element access
+    T &operator[] (const size_t i) {
+        return at(i);
+    }
+
+    T &at (const size_t i) {
+        if (i == 0){
+            return first->data;
+        }
+        if (i == count - 1){
+            return last->data;
+        }
+        component_vector<T> *current;
+        size_t current_i;
+        if(i <= count/2){
+            current = first;
+            current_i = 0;
+            for(; current_i < i; current_i++){
+                current = current->next;
+            }
+        }
+        else {
+            current = last;
+            current_i = count - 1;
+            for(; current_i > i; current_i--){
+                current = current->preveus;
+            }
+        }
+        return current->data;
+    }
+    // Modifiers
+    void push_back(const T& val) {
+        component_vector<T> *current = new component_vector<T>;
+        current->data = val;
+        current->preveus = last;
+        current->next = nullptr;
+
+        if (last != 0){
+            last->next = current;
+        }
+        if (count == 0){
+            first = last = current;
+        }
+        else {
+            last = current;
+        }
+
+        count++;
+    }
+    // Allocator
+
+    // Non-member function overloads
+
+
+
+//    T &at(size_t i) {
+//        if (i == 0){
+//            return first->data;
+//        }
+//        if (i == count - 1){
+//            return last->data;
+//        }
+//        component_vector<T> *current;
+//        size_t current_i;
+//        if(i <= count/2){
+//            current = first;
+//            current_i = 0;
+//            for(; current_i < i; current_i++){
+//                current = current->next;
+//            }
+//        }
+//        else {
+//            current = last;
+//            current_i = count - 1;
+//            for(; current_i > i; current_i--){
+//                current = current->preveus;
+//            }
+//        }
+//        return current->data;
+//    }
+
+
+//    ~StepanVector();
+//    int get_value(component_vector *node);
+//    int get_count();
+//    bool empty();
+//    component_vector *push(int data, component_vector *node);
+//    component_vector *clear_node(component_vector *node);
+//    component_vector *at(int i);
+//    void print(bool tail = true);
+//    void push_first(int data);
+//    void push_last(int data);
+//    void clear();
 };
 
 
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -3,18 +3,25 @@
 
 using namespace std;
 
+struct test {
+    int a;
+};
+
 int main(){
-    StepanVector lst;
+    test A; A.a = 10;
+    StepanVector<test> lst;
     cout << "start\n";
-    for (int i = 0; i < 10; i++){
-        lst.push_first(i);
-    }
-    lst.print();
-    lst.print(false);
-    cout << endl;
-    for (int i = 9; i >= 0; i--){
-        cout << lst.at(i)->data << " ";
-    }
+    lst.push_back(A);
+    cout << lst[0].a << endl;
+//    for (int i = 0; i < 10; i++){
+//        lst.push_first(i);
+//    }
+//    lst.print();
+//    lst.print(false);
+//    cout << endl;
+//    for (int i = 9; i >= 0; i--){
+//        cout << lst.at(i)->data << " ";
+//    }
 
 //    for (int i = 0; i < 10; i++){
 //        lst.push_last(i);
