fd2f0f556573cb807ce5bd2a8edc2225f0c8497a
khosta77 <puwerfulpants@gmail.com>
2022-07-13 13:09:59 +0300

    Добавлены базовые конструкторы, изменены некоторые методы

--- project/Vector/stepanvector.h
+++ project/Vector/stepanvector.h
@@ -30,16 +30,51 @@ public:
         count = 0;
     }
 
-//    StepanVector (const StepanVector<T> &SV);
-//    StepanVector<T> &operator= (const StepanVector<T> &SV);
-//    ~StepanVector();
+    StepanVector (const StepanVector<T> &SV) {
+        this->first = this->last = NULL;
+        this->count = 0;
+
+
+        for (size_t i = 0; i < SV.size(); i++) {
+            this->push_back(SV[i]);
+        }
+    }
+
+    StepanVector<T> &operator= (const StepanVector<T> &SV) {
+        if (this == &SV) {
+            return *this;
+        }
+
+        this->clear();
+
+        for (size_t i = 0; i < SV.size(); i++) {
+            this->push_back(SV[i]);
+        }
+        return *this;
+    }
+
+    ~StepanVector() {
+        while (first) {
+            component_vector<T> *newfirst = first->next;
+            delete first;
+            first = newfirst;
+        }
+        delete first;
+    }
 
     // Iterators
 
     // Capacity
+    size_t size() const {
+        return this->count;
+    }
 
     // Element access
     T &operator[] (const size_t i) {
+        return at(i);
+    }
+
+    T &operator[] (const size_t i) const {
         return at(i);
     }
 
@@ -47,28 +82,63 @@ public:
         if (i == 0){
             return first->data;
         }
+
         if (i == count - 1){
             return last->data;
         }
+
         component_vector<T> *current;
         size_t current_i;
         if(i <= count/2){
             current = first;
             current_i = 0;
+
             for(; current_i < i; current_i++){
                 current = current->next;
             }
-        }
-        else {
+        } else {
             current = last;
             current_i = count - 1;
+
             for(; current_i > i; current_i--){
                 current = current->preveus;
             }
         }
         return current->data;
     }
+
+    T &at (const size_t i) const {
+        if (i == 0){
+            return first->data;
+        }
+
+        if (i == count - 1){
+            return last->data;
+        }
+
+        component_vector<T> *current;
+        size_t current_i;
+        if(i <= count/2){
+            current = first;
+            current_i = 0;
+
+            for(; current_i < i; current_i++){
+                current = current->next;
+            }
+        } else {
+            current = last;
+            current_i = count - 1;
+
+            for(; current_i > i; current_i--){
+                current = current->preveus;
+            }
+        }
+        return current->data;
+    }
+
     // Modifiers
+//    void assign()
+
     void push_back(const T& val) {
         component_vector<T> *current = new component_vector<T>;
         current->data = val;
@@ -78,15 +148,36 @@ public:
         if (last != 0){
             last->next = current;
         }
+
         if (count == 0){
             first = last = current;
-        }
-        else {
+        } else {
             last = current;
         }
 
         count++;
     }
+
+//    void pop_back();
+//    void insert();
+//    void erase();
+//    void swap();
+
+    void clear() {
+        while (first) {
+            component_vector<T> *newfirst = first->next;
+            delete first;
+            first = newfirst;
+        }
+
+        count = 0;
+        first = last = NULL;
+    }
+
+//    void emplace();
+//    void emplace_back();
+
+
     // Allocator
 
     // Non-member function overloads
