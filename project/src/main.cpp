40aefc037e22b29fa64db5889a7aa5d574c167b2
khosta77 <puwerfulpants@gmail.com>
2022-07-22 15:22:45 +0300

    ...

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <list>
 #include "../List/stepandoublelist.h"
-#include "../List/stepanlist.h"
 
 using namespace std;
 
 int main(){
+    SList<int> obj;
+    const int SIZE = 10;
+    for (int i = 0; i < SIZE; i++) {
+        obj.push_back(i);
+    }
+    obj.print();
 
+    obj.pop_back();
 
+    obj.print();
+    cout << endl;
     return 0;
 }
