763b7cc704c0b5e9adb05a398cb8034d5351497b
khosta77 <puwerfulpants@gmail.com>
2022-07-25 13:26:14 +0300

    Собран стек

--- Stack/SStack.h
+++ Stack/SStack.h
@@ -3,56 +3,113 @@
 
 #include <iostream>
 
-struct component_stack {
-    int data;
-    component_stack *next;
-};
+namespace sstd {
 
-class StepanStack {
-private:
-    component_stack* first;
-public:
-    StepanStack() {
-        first = NULL;
-    }
-    bool empty() {
-        return first == NULL;
-    }
-    void push(int data) {
-        component_stack *newstack = new component_stack;
-        newstack->data = data;
-        newstack->next = first;
-        first = newstack;
-    }
-    int top() {
-        if(!empty()){
-            return first->data;
-        }
-        return 0;
-    }
-    void pop() {
-        if (!empty()){
-            component_stack *newfirst = first->next;
-            delete first;
-            first = newfirst;
-        }
-    }
-    size_t size() {
-        std::size_t size = 0;
-        if(!empty()){
-            component_stack *current = first;
-            while (current) {
-                size++;
-                current = current->next;
+    template<typename T>
+    struct stack_node {
+        T data;
+        stack_node<T> *next;
+    };
+
+    template<typename T>
+    class SStack {
+    private:
+        stack_node<T> *first;
+
+    protected:
+        void clear() {
+            while (!empty()) {
+                pop();
             }
         }
-        return size;
-    }
-    ~StepanStack() {
-        while (!empty()) {
-            pop();
+
+        void push_end(T data) {
+            stack_node<T> *new_stack = first;
+            if(new_stack){
+                while (new_stack->next) {
+                    new_stack = new_stack->next;
+                }
+                new_stack->next = new stack_node<T>;
+                new_stack = new_stack->next;
+                new_stack->data = data;
+                new_stack->next = NULL;
+            }
+            else {
+                first = new stack_node<T>;
+                first->data = data;
+                first->next = NULL;
+            }
         }
-    }
-};
+    public:
+//------------------------------------------------------------------------------------------------------------
+        //  Member functions
+        SStack() {
+            first = NULL;
+        }
+
+        SStack(const SStack<T> &other) {
+            first = NULL;
+            this = other;
+        }
+
+        ~SStack() {
+            clear();
+            delete first;
+        }
+
+        SStack<T> &operator=(const SStack<T> &other) {
+            clear();
+            stack_node<T> *_buf_stack_node = other.first;
+            for (size_t i = 0; i < other.size(); i++) {
+                push(_buf_stack_node->data);
+                _buf_stack_node = _buf_stack_node->next;
+            }
+
+            return *this;
+        }
+//------------------------------------------------------------------------------------------------------------
+        //  Element access
+        T top() {
+            if (!empty()) {
+                return first->data;
+            }
+            return 0;
+        }
+//------------------------------------------------------------------------------------------------------------
+        //  Capacity
+        bool empty() const {
+            return first == NULL;
+        }
+
+        size_t size() const {
+            size_t size = 0;
+            if (!empty()) {
+                stack_node<T> *current = first;
+                while (current) {
+                    size++;
+                    current = current->next;
+                }
+            }
+            return size;
+        }
+//------------------------------------------------------------------------------------------------------------
+        //  Modifiers
+        void push(T data) {
+            stack_node<T> *new_stack = new stack_node<T>;
+            new_stack->data = data;
+            new_stack->next = first;
+            first = new_stack;
+        }
+
+        void pop() {
+            if (!empty()) {
+                stack_node<T> *new_first = first->next;
+                delete first;
+                first = new_first;
+            }
+        }
+    };
+
+};  // Stepan Standart Template Data
 
 #endif // STEPANSTACK_H
